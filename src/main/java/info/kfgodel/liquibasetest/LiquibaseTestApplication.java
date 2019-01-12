6e73c5d380e637bd7e035badbb452f859dbf20c2
Dario L Garcia <dario.garcia@10pines.com>
2019-01-12 01:57:21 -0300

    Commit inicial tomado desde initializr para liquibase con jpa y postgres

---
+++ src/main/java/info/kfgodel/liquibasetest/LiquibaseTestApplication.java
@@ -0,0 +1,14 @@
+package info.kfgodel.liquibasetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseTestApplication.class, args);
+	}
+
+}
+
